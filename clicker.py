d84141fa459505c2f2a78506468967ea6c33140e
Devin Gerloff <devin.gerloff.03@student.ssttx.org>
2025-11-14 09:51:18 -0600

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -1,39 +1,9 @@
-# === imports === #
-import turtle as turt
+import turtle
 
-# === initalize constants/lists === #
-screen = turt.Screen()
-APC = 1
-TA = 0
-CA = 0
-APS = 0
-font = ("roboto", 18, "bold")
-
-# === Turtle Initialization === #
-# - score display turtle - #
-al_score = turt.Turtle()
-al_score.penup()
-al_score.hideturtle()
-al_score.goto(200,200)
-al_score.write(f"Score: {CA}", align="center", font=font)
-# - upgrades - #
-upgrades = []
-for _ in range(12):
-    upgrade = turt.Turtle()
-    upgrade.penup()
-    upgrades.append(upgrade)
-
-# === Game Functions === #
-def click(x,y):
-    global TA, APC, CA
-    TA += APC
-    CA += APC
-    al_score.clear()
-    al_score.write(f"Score: {CA}", align="center", font=font)
-    screen.update()
-    
-# === Run Game === #
-screen.onscreenclick(click)
-
+def click(x, y, alberts_per_click, current_alberts, total_alberts):
+    current_alberts += alberts_per_click
+    total_alberts += alberts_per_click
+    return current_alberts
 
 screen.mainloop()
+
